ca4771d2eb4d05afb365742e54a2472624c7ff06
drphrozen <drphrozen@860a7497-9de2-8477-7387-c7b486605186>
2010-09-29 12:24:35 +0000

    Added CheckBoxParent, for grouping checkboxes

---
+++ WinFormPlusPlus/CheckBoxParentExample/MainForm.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows.Forms;
+using WinFormPlusPlus;
+
+namespace CheckBoxCollection
+{
+  public partial class MainForm : Form
+  {
+    public MainForm()
+    {
+      InitializeComponent();
+      var checkBoxes = new CheckBox[10];
+      for (var i = 0; i < checkBoxes.Length; i++)
+      {
+        checkBoxes[i] = new CheckBox { Text = i.ToString() };
+        flowLayoutPanel1.Controls.Add(checkBoxes[i]);
+      }
+
+      var parentAll = new CheckBoxParent { Text = "All" };
+      foreach (var checkBox in checkBoxes)
+        parentAll.AddChildCheckBox(checkBox);
+      flowLayoutPanel1.Controls.Add(parentAll);
+      
+      var parentLast5 = new CheckBoxParent { Text = "Last 5" };
+      for (var i = 5; i < checkBoxes.Length; i++)
+        parentLast5.AddChildCheckBox(checkBoxes[i]);        
+      flowLayoutPanel1.Controls.Add(parentLast5);
+
+      var parentInParent = new CheckBoxParent { Text = "Parent in Parent" };
+      parentInParent.AddChildCheckBox(checkBoxes[0]);
+      parentInParent.AddChildCheckBox(parentLast5);
+      flowLayoutPanel1.Controls.Add(parentInParent);
+    }
+  }
+}
---
+++ WinFormPlusPlus/WinFormPlusPlus/CheckBoxParent.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace WinFormPlusPlus
+{
+  public class CheckBoxParent : CheckBox
+  {
+    readonly List<CheckBox> _checkBoxes = new List<CheckBox>();
+    private bool _disableEvents;
+
+    public CheckBoxParent() {
+      CheckStateChanged += CheckBoxParent_CheckStateChanged;
+    }
+
+    void CheckBoxParent_CheckStateChanged(object sender, EventArgs e)
+    {
+      if (_disableEvents) return;
+      _disableEvents = true;
+      var checkAll = CheckState != CheckState.Unchecked;
+      foreach (var checkBox in _checkBoxes) {
+        checkBox.Checked = checkAll;
+      }
+      _disableEvents = false;
+    }
+
+    public void AddChildCheckBox(CheckBox checkBox) {
+      _checkBoxes.Add(checkBox);
+      checkBox.CheckStateChanged += CheckBox_CheckStateChanged;
+    }
+
+    void CheckBox_CheckStateChanged(object sender, EventArgs e)
+    {
+      if (_disableEvents) return;
+      _disableEvents = true;
+      CheckState = GetState();
+      _disableEvents = false;
+    }
+
+    private CheckState GetState() {
+      var isChecked = _checkBoxes[0].CheckState;
+      return _checkBoxes.Any(checkBox => checkBox.CheckState != isChecked) ? CheckState.Indeterminate : isChecked;
+    }
+  }
+}
